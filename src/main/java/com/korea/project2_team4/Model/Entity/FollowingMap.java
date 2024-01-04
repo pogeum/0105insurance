a3aa97e9de1f1a54c7f11faa47d84fd50f074ff0
unknown <qhs3012@gmail.com>
2024-01-04 21:41:05 +0900

    dd

--- src/main/java/com/korea/project2_team4/Model/Entity/FollowingMap.java
+++ src/main/java/com/korea/project2_team4/Model/Entity/FollowingMap.java
@@ -3,29 +3,29 @@ package com.korea.project2_team4.Model.Entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
-public class Follow {
+public class FollowingMap {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //팔로우 하는 사용자 프로필
+    //팔로우 하는 사용자 ID
     @ManyToOne
     @JoinColumn(name = "follower_id")
-    private Profile followerProfile;
+    private Member follower;
 
     //팔로우 받는 사용자 ID
     @ManyToOne
     @JoinColumn(name = "following_id")
-    private Profile followingProfile;
+    private Member followee;
 
     //팔로우 한 시간
     private LocalDateTime followedAt;
+}
 }
