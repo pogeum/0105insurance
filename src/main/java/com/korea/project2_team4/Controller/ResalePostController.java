2461c05e099d8139c1effe4a1717bbce9be3a5bd
kimjinku <kjk10271215@gmail.com>
2024-01-16 17:54:36 +0900

    team2

--- src/main/java/com/korea/project2_team4/Controller/ResalePostController.java
+++ src/main/java/com/korea/project2_team4/Controller/ResalePostController.java
@@ -1,23 +1,27 @@
 package com.korea.project2_team4.Controller;
 
-import com.korea.project2_team4.Model.Entity.Member;
-import com.korea.project2_team4.Model.Entity.Post;
-import com.korea.project2_team4.Model.Entity.ResalePost;
-import com.korea.project2_team4.Model.Entity.Tag;
+import com.korea.project2_team4.Model.Entity.*;
 import com.korea.project2_team4.Model.Form.PostForm;
+import com.korea.project2_team4.Model.Form.ResalePostForm;
 import com.korea.project2_team4.Repository.PostRepository;
 import com.korea.project2_team4.Service.*;
+import jakarta.servlet.http.HttpSession;
 import lombok.Builder;
 import org.springframework.boot.Banner;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.NoSuchAlgorithmException;
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -41,7 +45,7 @@ public class ResalePostController {
     @GetMapping("/main")
     public String resalePost(Model model, @RequestParam(value = "page", defaultValue = "0") int page){
         Page<ResalePost> resalePostList = resalePostService.resalePostList(page);
-        model.addAttribute("resalePostList",resalePostList);
+        model.addAttribute("paging",resalePostList);
         return "resale_main";
     }
 
@@ -59,6 +63,41 @@ public class ResalePostController {
             model.addAttribute("post", resalePost);
         }
         return "ResalePost/resalePostDetail_form";
+    }
+    @GetMapping("/createResalePost")
+    public String createPost(Model model, ResalePostForm resalePostForm) {
+        model.addAttribute("resalePostForm",resalePostForm);
+        return "ResalePost/createResalePost_form";
+    }
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/createResalePost")
+    public String createResalePost(Principal principal, ResalePostForm resalePostForm, BindingResult bindingResult
+            , @RequestParam(value = "imageFiles", required = false) List<MultipartFile> imageFiles
+    ) throws IOException, NoSuchAlgorithmException {
+        ResalePost resalePost = new ResalePost();
+
+        Member sitemember = this.memberService.getMember(principal.getName());
+        resalePost.setTitle(resalePostForm.getTitle());
+        resalePost.setPrice(resalePostForm.getPrice());
+        resalePost.setContent(resalePostForm.getContent());
+        resalePost.setCreateDate(LocalDateTime.now());
+        resalePost.setSeller(sitemember.getProfile());
+        if (imageFiles != null && !imageFiles.isEmpty()) {
+            imageService.uploadResalePostImage(imageFiles, resalePost);
+        }
+
+        resalePostService.save(resalePost);
+
+
+//        String encodedCategory;
+//        try {
+//            encodedCategory = URLEncoder.encode(postForm.getCategory(), "UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            // 예외 처리 필요
+//            encodedCategory = "";
+//        }
+
+        return "redirect:/resalePost/detail/"+ resalePost.getId() +"/0";
     }
 }
 
